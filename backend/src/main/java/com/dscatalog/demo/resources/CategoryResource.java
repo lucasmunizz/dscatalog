ba1c8bec3db5739c1455664bcd192458507aecec
Lucas Muniz <lucaslima1048@gmail.com>
2023-07-14 14:50:20 -0300

    Repository, service, application properties

--- backend/src/main/java/com/dscatalog/demo/resources/CategoryResource.java
+++ backend/src/main/java/com/dscatalog/demo/resources/CategoryResource.java
@@ -3,21 +3,27 @@ package com.dscatalog.demo.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dscatalog.demo.entities.Category;
+import com.dscatalog.demo.repositories.CategoryRepository;
+import com.dscatalog.demo.services.CategoryService;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 	
+	@Autowired
+	private CategoryService service;
+	
+	@GetMapping
 	public ResponseEntity<List<Category>> findAll(){
-		List<Category> list = new ArrayList<>();
-		list.add(new Category(1L, "Books"));
-		list.add(new Category(2L, "Eletronics"));
-		return ResponseEntity.ok().body(list);
+		List<Category> categories = service.findAll();
+		return ResponseEntity.ok().body(categories);
 	}
 	
 }
