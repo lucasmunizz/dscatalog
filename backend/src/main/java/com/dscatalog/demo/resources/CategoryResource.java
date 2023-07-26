a7c7621cccb40bbeab5ea551f9cec79fa6998856
Lucas Muniz <lucaslima1048@gmail.com>
2023-07-26 16:44:22 -0300

    refactor pageable

--- backend/src/main/java/com/dscatalog/demo/resources/CategoryResource.java
+++ backend/src/main/java/com/dscatalog/demo/resources/CategoryResource.java
@@ -1,10 +1,12 @@
 package com.dscatalog.demo.resources;
 
+
 import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -14,7 +16,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -29,15 +30,8 @@ public class CategoryResource {
 	private CategoryService service;
 	
 	@GetMapping
-	public ResponseEntity<Page<CategoryDTO>> findAll(
-			@RequestParam(value = "page", defaultValue = "0") Integer page,
-			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
-			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy,
-			@RequestParam(value = "direction", defaultValue = "DESC") String direction
-			){
-		
-		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
-		Page<CategoryDTO> categories = service.findAll(pageRequest);
+	public ResponseEntity<Page<CategoryDTO>> findAll(Pageable pageable){
+		Page<CategoryDTO> categories = service.findAll(pageable);
 		return ResponseEntity.ok().body(categories);
 	}
 	
